4ba6e56757016f8b94a2917b9ba1b66c7a7b2f64
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-04-06 13:37:13 +0300

    EP-2156

--- src/views/RouteTekstikappale.vue
+++ src/views/RouteTekstikappale.vue
@@ -12,35 +12,57 @@
           <ep-content v-model="data.teksti" layout="normal" :is-editable="isEditing"></ep-content>
         </div>
 
-        <div v-if="koodistoryhmat">
+        <div v-if="koodistoryhmat" class="koodistoryhmat">
           <h3>{{$t('koodistot')}}</h3>
 
-          <p v-if="!data.koodit && !isEditing">{{$t('tekstikappaletta-ei-ole-liitetty-koodistoihin')}}</p>
+          <div v-if="!isEditing">
+            <p v-if="(!data.koodit || data.koodit.length === 0)">{{$t('tekstikappaletta-ei-ole-liitetty-koodistoihin')}}</p>
+            <p v-else>{{$t('tekstikappale-on-liitetty-koodistoihin')}}</p>
+          </div>
+          <div v-else>
+            <p>{{$t('tekstikappale-koodisto-ohje')}} <br/>
+              <router-link :to="{ name: 'perusteenTiedot' }" exact>
+                {{ $t('tekstikappale-koodisto-ohje-linkki') }}
+              </router-link>
+            </p>
+          </div>
 
-          <div v-if="isEditing">
-
-            <div v-for="(koodistoryhma, index) in koodistoryhmat" :key="'koodistoryhma'+index">
+          <div v-if="(data.koodit && data.koodit.length > 0) || isEditing">
+            <div v-for="(koodistoryhma, index) in koodistot" :key="'koodistoryhma'+index" class="koodistoryhma">
 
               <h4>{{$t(koodistoryhma.ryhma)}}</h4>
 
-              <div v-for="(koodisto, index) in koodistoryhma.koodistot" :key="'koodisto'+index">
-                <h4>{{$t(koodisto)}}</h4>
+              <div class="koodistot">
+                <div v-for="(koodisto, index) in koodistoryhma.koodistot" :key="'koodisto'+index">
 
-                <div v-for="(koodi, index) in koodistonKoodit(koodisto)" :key="koodisto+'koodi'+index">
-                  {{$kaanna(koodi.nimi)}}
+                  <div v-if="koodisto.koodit.length > 0 || isEditing" class="koodisto">
+                    <h4>{{$t(koodisto.koodisto)}}</h4>
+
+                    <b-table
+                      sortable
+                      striped
+                      :fields="fields"
+                      :items="koodisto.koodit"
+                      v-if="koodisto.koodit.length > 0">
+
+                      <template v-slot:cell(delete)="data">
+                        <fas icon="roskalaatikko" class="roskalaatikko" @click="poistaKoodi(data.item)"/>
+                      </template>
+
+                    </b-table>
+
+                    <ep-koodisto-select :store="storet[koodisto.koodisto]" @add="lisaaKoodit" v-if="isEditing" :multiple="true">
+                      <template v-slot:koodisto>({{koodisto.koodisto}})</template>
+                      <template #default="{ open }">
+                        <ep-button @click="open" icon="plus" variant="outline">
+                          {{ $t('lisaa-'+koodisto.koodisto) }}
+                        </ep-button>
+                      </template>
+                    </ep-koodisto-select>
+                  </div>
                 </div>
-
-                <ep-koodisto-select :store="storet[koodisto]" @add="lisaaKoodi">
-                  <template #default="{ open }">
-                    <ep-button @click="open" icon="plus" variant="outline">
-                      {{ $t('lisaa-'+koodisto) }}
-                    </ep-button>
-                  </template>
-                </ep-koodisto-select>
-
               </div>
             </div>
-
           </div>
         </div>
       </template>
@@ -63,6 +85,7 @@ import EpKoodistoSelect from '@shared/components/EpKoodistoSelect/EpKoodistoSele
 import { KoodistoSelectStore } from '@shared/components/EpKoodistoSelect/KoodistoSelectStore';
 import EpButton from '@shared/components/EpButton/EpButton.vue';
 import * as _ from 'lodash';
+import { KoodiDto } from '../../eperusteet-frontend-utils/vue/src/generated/eperusteet';
 
 interface koodistoryhma {
   ryhma: string;
@@ -87,10 +110,6 @@ export default class RouteTekstikappale extends Vue {
   private koodistoryhmat!: koodistoryhma[];
 
   private store: EditointiStore | null = null;
-
-  private tutkintonimikkeet = [] as any[];
-
-  private koodit = [] as any[];
   private storet = {};
 
   mounted() {
@@ -117,6 +136,22 @@ export default class RouteTekstikappale extends Vue {
     return this.perusteStore.perusteId.value;
   }
 
+  get koodistot() {
+    return _.chain(this.koodistoryhmat)
+      .map(koodistoRyhma => {
+        return {
+          ryhma: koodistoRyhma.ryhma,
+          koodistot: _.map(koodistoRyhma.koodistot, koodisto => {
+            return {
+              koodisto,
+              koodit: this.koodistonKoodit(koodisto),
+            };
+          }),
+        };
+      })
+      .value();
+  }
+
   @Watch('tekstikappaleId', { immediate: true })
   async onParamChange(id: string, oldId: string) {
     if (!id || id === oldId) {
@@ -130,11 +165,12 @@ export default class RouteTekstikappale extends Vue {
 
   private koodistoStore(koodistoNimi) {
     return new KoodistoSelectStore({
-      async query(query: string, sivu = 0) {
+      async query(query: string, sivu = 0, onlyValidKoodis = true) {
         return (await Koodisto.kaikkiSivutettuna(koodistoNimi, query, {
           params: {
             sivu,
             sivukoko: 10,
+            onlyValidKoodis: onlyValidKoodis,
           },
         })).data as any;
       },
@@ -142,23 +178,98 @@ export default class RouteTekstikappale extends Vue {
   }
 
   koodistonKoodit(koodisto) {
-    return _.chain(this.koodit)
+    return _.chain(this.store?.data.value.koodit)
       .filter(koodi => koodi.koodisto === koodisto)
-      .map('koodi')
       .value();
   }
 
-  lisaaKoodi(koodi) {
-    this.koodit = [
-      ...this.koodit,
-      {
-        koodi,
-        koodisto: koodi.koodisto.koodistoUri,
+  lisaaKoodit(koodit: any[]) {
+    const kooditKoodistolla = _.chain(koodit)
+      .filter(koodi => !_.includes(_.map(this.store?.data.value.koodit, 'uri'), koodi.uri))
+      .map(koodi => {
+        return {
+          ...koodi,
+          koodisto: koodi.koodisto.koodistoUri,
+        };
+      })
+      .value();
+
+    this.store?.setData({
+      ...this.store?.data.value,
+      koodit: [
+        ...this.store?.data.value.koodit,
+        ...kooditKoodistolla,
+      ],
+    });
+  }
+
+  poistaKoodi(koodi) {
+    this.store?.setData({
+      ...this.store?.data.value,
+      koodit: _.filter(this.store?.data.value.koodit, dataKoodi => dataKoodi.uri !== koodi.uri),
+    });
+  }
+
+  get fields() {
+    let fields = [{
+      key: 'nimi',
+      label: this.$t('nimi'),
+      sortable: true,
+      formatter: (value, key, item) => {
+        return this.$kaanna(item.nimi);
       },
-    ];
+    }, {
+      key: 'arvo',
+      sortable: true,
+      label: this.$t('koodi'),
+      thStyle: 'width: 10%',
+    }];
+
+    if (this.store?.isEditing.value) {
+      return [
+        ...fields,
+        {
+          key: 'delete',
+          label: '',
+          sortable: false,
+          thClass: 'border-0',
+          thStyle: 'width: 1%',
+        },
+      ];
+    }
+
+    return fields;
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  @import "@shared/styles/_variables.scss";
+
+  .koodistoryhmat {
+    margin-top: 50px;
+
+    .koodistoryhma {
+      margin-top: 30px;
+
+      .koodistot {
+        padding: 20px;
+        border: 1px solid $gray-lighten-4;
+
+        .koodisto {
+          margin-bottom: 30px;
+
+          .roskalaatikko {
+            color: $paletti-blue;
+            cursor: pointer;
+            margin: 0px 10px;
+          }
+
+        }
+      }
+
+    }
+
+  }
+
 </style>
