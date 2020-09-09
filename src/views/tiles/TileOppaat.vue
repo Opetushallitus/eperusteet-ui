33c79be36bef879b03122b956b888ba6e396b6ac
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-09-09 15:34:55 +0300

    EP-2320

--- src/views/tiles/TileOppaat.vue
+++ src/views/tiles/TileOppaat.vue
@@ -4,30 +4,58 @@
     <span>{{ $t('oppaat') }}</span>
   </template>
   <template slot="content">
-    <ep-spinner v-if="isLoading"></ep-spinner>
+    <ep-spinner v-if="!oppaat"></ep-spinner>
     <div v-else>
-      <p>{{ $t('tile-oppaat-kuvaus') }}</p>
+      <p v-if="oppaat.length === 0">{{ $t('ei-oppaita') }}</p>
+
+      <div v-else class="text-left ml-5">
+        <div v-for="(opas, index) in viimeisimmatOppaat" :key="'opas'+index">
+          {{opas.nimi}}
+        </div>
+        <ep-button variant="link" v-if="kokonaismaara > viimeisimmatOppaat.length" buttonClass="pl-0 btn-sm" class="no-padding">
+          {{kokonaismaara - viimeisimmatOppaat.length}} {{$t('muuta-opasta')}}
+        </ep-button>
+      </div>
     </div>
   </template>
 </EpHomeTile>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component, Prop } from 'vue-property-decorator';
 import EpHomeTile from '@shared/components/EpHomeTiles/EpHomeTile.vue';
 import EpSpinner from '@shared/components/EpSpinner/EpSpinner.vue';
+import { PerusteetStore } from '@/stores/PerusteetStore';
+import * as _ from 'lodash';
+import EpButton from '@shared/components/EpButton/EpButton.vue';
 
 @Component({
   components: {
     EpHomeTile,
     EpSpinner,
+    EpButton,
   },
 })
 export default class TileOppaat extends Vue {
+  @Prop({ required: true })
+  private perusteOppaatStore!: PerusteetStore;
+
   private isLoading = false;
 
   async mounted() {
-    this.isLoading = false;
+    await this.perusteOppaatStore.updateQuery({});
+  }
+
+  get oppaat() {
+    return this.perusteOppaatStore.projects.value?.data || null;
+  }
+
+  get kokonaismaara() {
+    return this.perusteOppaatStore.projects.value?.kokonaismäärä || null;
+  }
+
+  get viimeisimmatOppaat() {
+    return _.take(this.oppaat, 3);
   }
 }
 </script>
--- src/views/tiles/TileVirheellisetPerusteet.vue
+++ src/views/tiles/TileVirheellisetPerusteet.vue
@@ -4,29 +4,59 @@
     <span>{{ $t('virheelliset-perusteet') }}</span>
   </template>
   <template slot="content">
-    <ep-spinner v-if="isLoading"></ep-spinner>
+    <ep-spinner v-if="!validations"></ep-spinner>
     <div v-else>
-      <p>{{ $t('tile-virheelliset-kuvaus') }}</p>
+      <p v-if="validations.length === 0">{{ $t('ei-virheellisia-perusteita') }}</p>
+
+      <div v-else class="text-left ml-5">
+        <div v-for="(validation, index) in validations" :key="'opas'+index" class="row ">
+          <div class="col-2 font-size-08 mr-0 pr-0">{{validation.infot.length}} {{$t('virhetta')}}</div>
+          <div class="col-10 ml-0">{{validation.perusteprojekti.nimi}}</div>
+        </div>
+
+        <div class="row" v-if="kokonaismaara > validations.length">
+          <div class="col-2" />
+          <div class="col-10">
+            <ep-button variant="link" buttonClass="pl-0 btn-sm" class="no-padding">
+              {{kokonaismaara - validations.length}} {{$t('muuta-perusteprojektia')}}
+            </ep-button>
+          </div>
+        </div>
+      </div>
     </div>
+
   </template>
 </EpHomeTile>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component, Prop } from 'vue-property-decorator';
 import EpHomeTile from '@shared/components/EpHomeTiles/EpHomeTile.vue';
 import EpSpinner from '@shared/components/EpSpinner/EpSpinner.vue';
+import { VirheellisetPerusteetStore } from '@/stores/VirheellisetPerusteetStore';
+import EpButton from '@shared/components/EpButton/EpButton.vue';
 
 @Component({
   components: {
     EpHomeTile,
     EpSpinner,
+    EpButton,
   },
 })
 export default class TileVirheellisetPerusteet extends Vue {
-  private isLoading = false;
+  @Prop({ required: true })
+  private virheellisetPerusteetStore!: VirheellisetPerusteetStore;
 
-  async mounted() {
+  mounted() {
+    this.virheellisetPerusteetStore.updateFilters(0, 2);
+  }
+
+  get validations() {
+    return this.virheellisetPerusteetStore.validations.value?.data || null;
+  }
+
+  get kokonaismaara() {
+    return this.virheellisetPerusteetStore.kokonaismaara.value;
   }
 }
 </script>
