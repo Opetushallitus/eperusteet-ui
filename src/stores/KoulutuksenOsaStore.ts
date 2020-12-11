3f63f22c6300a58a6e5fd0a207d5d53c948b339e
Mikael Pursiainen <mikael.pursiainen@gofore.com>
2020-12-11 15:12:02 +0200

    Create koulutuksen osa

--- src/stores/KoulutuksenOsaStore.ts
+++ src/stores/KoulutuksenOsaStore.ts
@@ -2,12 +2,12 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueCompositionApi, { reactive, computed } from '@vue/composition-api';
 
-import { required } from 'vuelidate/lib/validators';
+import { minLength, required } from 'vuelidate/lib/validators';
 import _ from 'lodash';
 
-import { Matala, Perusteenosat, Sisallot } from '@shared/api/eperusteet';
+import { KoulutuksenOsaDto, KoulutuksenOsaDtoKoulutusOsanKoulutustyyppiEnum, Matala, Perusteenosat, Sisallot } from '@shared/api/eperusteet';
 import { Revision } from '@shared/tyypit';
-import { allTranslations } from '@shared/validators/required';
+import { allTranslations, translated } from '@shared/validators/required';
 import { IEditoitava } from '@shared/components/EpEditointi/EditointiStore';
 
 import { PerusteStore } from '@/stores/PerusteStore';
@@ -58,9 +58,13 @@ export class KoulutuksenOsaStore implements IEditoitava {
     return this.koulutuksenosa.value;
   }
 
-  public async save(data: any) {
-    data.nimi = data.nimiKoodi.nimi;
-    const res = await Perusteenosat.updatePerusteenOsa(this.id.value!, data);
+  public async save(data: KoulutuksenOsaDto) {
+    if (this.isTuvaKoulutusTyyppi(data.koulutusOsanKoulutustyyppi as string)) {
+      data.nimi = data.nimiKoodi?.nimi;
+    } else if (!!data.nimiKoodi) {
+      data.nimiKoodi = undefined;
+    }
+    const res = await Perusteenosat.updatePerusteenOsa(this.id.value!, data as any);
 
     KoulutuksenOsaStore.config!.perusteStore!.updateNavigationEntry({
       id: this.koulutuksenosaId,
@@ -69,6 +73,14 @@ export class KoulutuksenOsaStore implements IEditoitava {
     });
 
     return res.data;
+  }
+
+  private isTuvaKoulutusTyyppi(type: string): boolean {
+    return type === _.chain(KoulutuksenOsaDtoKoulutusOsanKoulutustyyppiEnum.TUTKINTOKOULUTUKSEENVALMENTAVA)
+      .split('_')
+      .join('')
+      .toLower()
+      .value();
   }
 
   public async history() {
@@ -113,10 +125,17 @@ export class KoulutuksenOsaStore implements IEditoitava {
   public readonly validator = computed(() => {
     const julkaisukielet = KoulutuksenOsaStore.config!.perusteStore.julkaisukielet.value;
     return {
-      nimiKoodi: {
-        nimi: required,
+      koulutusOsanKoulutustyyppi: { required },
+      // nimiKoodi: {
+      //   nimi: required,
+      // },
+      nimi: translated(julkaisukielet),
+      laajuusMinimi: { required },
+      laajuusMaksimi: { required },
+      arvioinnit: {
+        'min-length': minLength(1),
+        required,
       },
-      kuvaus: allTranslations(julkaisukielet),
     };
   });
 
--- src/views/RouteKoulutuksenOsa.vue
+++ src/views/RouteKoulutuksenOsa.vue
@@ -3,8 +3,222 @@
     <template #header="{ data }">
       <h2 class="m-0">{{ $kaanna(data.nimi) || $t('nimeton-koulutuksen-osa') }}</h2>
     </template>
-     <template v-slot:default="{ data, isEditing, validation }">
-     </template>
+     <template #default="{ data, isEditing, validation }">
+       <b-row>
+         <b-col>
+           <b-form-group :label="$t('koulutustyyppi') + (isEditing ? ' *' : '')" required>
+            <template v-if="isEditing">
+              <b-form-radio
+                v-for="type in koulutusOsanKoulutustyypit"
+                :key="type"
+                class="ml-1"
+                v-model="data.koulutusOsanKoulutustyyppi"
+                :value="type"
+                name="koulutustyyppi"
+                :validation="validation.koulutusOsanKoulutustyyppi">
+                {{ $t(type) }}
+              </b-form-radio>
+            </template>
+            <template v-else>{{ $t(data.koulutusOsanKoulutustyyppi) }}</template>
+            <p v-if="!isEditing && !data.koulutusOsanKoulutustyyppi" class="font-italic">{{ $t('ei-asetettu') }}</p>
+          </b-form-group>
+         </b-col>
+       </b-row>
+      <b-row>
+        <b-col md="6">
+          <b-form-group :label="$t('koulutuksen-osan-nimi') + (isEditing ? ' *' : '')" required>
+            <EpKoodistoSelect
+              v-if="isTuvaKoulutusTyyppi(data.koulutusOsanKoulutustyyppi)"
+              :store="koodisto"
+              v-model="data.nimiKoodi"
+              :is-editing="isEditing"
+              :naytaArvo="false">
+              <template #default="{ open }">
+                <b-input-group>
+                  <b-form-input
+                    :value="data.nimiKoodi ? $kaanna(data.nimiKoodi.nimi) : ''"
+                    disabled>
+                  </b-form-input>
+                  <b-input-group-append>
+                    <b-button
+                      @click="open"
+                      icon="plus"
+                      variant="primary">
+                      {{ $t('hae-koodistosta') }}
+                    </b-button>
+                  </b-input-group-append>
+                </b-input-group>
+              </template>
+            </EpKoodistoSelect>
+            <EpInput
+              v-else
+              v-model="data.nimi"
+              :is-editing="isEditing"
+              :validation="validation.nimi"/>
+            <p v-if="!isEditing && !data.nimi" class="font-italic">{{ $t('ei-asetettu') }}</p>
+          </b-form-group>
+        </b-col>
+        <b-col md="4">
+          <b-form-group :label="$t('laajuus') + (isEditing ? ' *' : '')" required>
+            <div v-if="isEditing || !isEditing && data.laajuusMinimi && data.laajuusMaksimi" class="d-flex align-items-center">
+              <EpInput
+                v-model="data.laajuusMinimi"
+                :is-editing="isEditing"
+                :validation="validation.laajuusMinimi"
+                type="number"
+                min="0"/>
+              <span :class="isEditing ? 'mx-3' : 'mx-1'">-</span>
+              <EpInput
+                v-model="data.laajuusMaksimi"
+                :is-editing="isEditing"
+                :validation="validation.laajuusMaksimi"
+                type="number"
+                min="0"/>
+              <span class="ml-2">{{ $t('viikkoa') }}</span>
+            </div>
+            <p v-else-if="!isEditing" class="font-italic">{{ $t('ei-asetettu') }}</p>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="10">
+          <b-form-group :label="$t('kuvaus')">
+            <EpContent
+              v-if="isEditing || !isEditing && data.kuvaus"
+              v-model="data.kuvaus"
+              layout="normal"
+              :is-editable="isEditing"
+              :validation="validation.kuvaus"
+              :kuvaHandler="kuvaHandler"/>
+            <EpAlert v-if="!isEditing && !data.kuvaus" :text="$t('ei-sisaltoa')" />
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <hr>
+      <b-row>
+        <b-col>
+          <h3 class="mb-3">{{ $t('tavoitteet') }}</h3>
+          <b-form-group :label="$t('opiskelija')">
+            <template v-if="isEditing">
+              <draggable
+                v-bind="tavoitteetOptions"
+                tag="div"
+                v-model="data.tavoitteet">
+                <b-row v-for="(tavoite, i) in data.tavoitteet" :key="tavoite.id" class="pb-2">
+                  <b-col cols="12" lg="10">
+                    <EpInput
+                      v-model="data.tavoitteet[i]"
+                      :is-editing="isEditing"
+                      :disabled="tavoite.uri !== undefined">
+                      <div class="order-handle m-2" slot="left">
+                        <fas icon="grip-vertical"/>
+                      </div>
+                    </EpInput>
+                  </b-col>
+                  <b-col cols="1" v-if="isEditing">
+                    <fas
+                      icon="roskalaatikko"
+                      class="default-icon clickable mt-2"
+                      @click="onRemoveListItem(tavoite, 'tavoitteet')"/>
+                  </b-col>
+                </b-row>
+              </draggable>
+              <EpButton
+                variant="outline"
+                icon="plus"
+                @click="onAddListItem('tavoitteet')"
+                v-if="isEditing">
+                {{ $t('lisaa-tavoite') }}
+              </EpButton>
+            </template>
+            <template v-else-if="data.tavoitteet.length > 0">
+              <ul>
+                <li v-for="tavoite in data.tavoitteet" :key="tavoite.id">
+                  {{$kaanna(tavoite)}}
+                </li>
+              </ul>
+            </template>
+            <p v-else-if="!isEditing" class="font-italic">{{ $t('ei-asetettu') }}</p>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <hr>
+      <b-row>
+        <b-col md="10">
+          <h3>{{ $t('keskeinen-sisalto') }}</h3>
+          <b-form-group>
+            <EpContent
+              v-if="isEditing || !isEditing && data.keskeinenSisalto"
+              v-model="data.keskeinenSisalto"
+              layout="normal"
+              :is-editable="isEditing"
+              :kuvaHandler="kuvaHandler"/>
+            <EpAlert v-if="!isEditing && !data.keskeinenSisalto" :text="$t('ei-sisaltoa')" />
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <hr>
+      <b-row>
+        <b-col>
+          <h3 class="mb-3">{{ $t('arviointi') }}</h3>
+          <b-form-group :label="$t('koulutuksen-osa-hyvaksytty-kun-opiskelija') + (isEditing ? ' *' : '')" required>
+            <template v-if="isEditing">
+              <draggable
+                v-bind="arvioinnitOptions"
+                tag="div"
+                v-model="data.arvioinnit">
+                <b-row v-for="(arviointi, i) in data.arvioinnit" :key="arviointi.id" class="pb-2">
+                  <b-col cols="12" lg="10">
+                    <EpInput
+                      v-model="data.arvioinnit[i]"
+                      :is-editing="isEditing"
+                      :disabled="arviointi.uri !== undefined">
+                      <div class="order-handle m-2" slot="left">
+                        <fas icon="grip-vertical"/>
+                      </div>
+                    </EpInput>
+                  </b-col>
+                  <b-col cols="1" v-if="isEditing">
+                    <fas
+                      icon="roskalaatikko"
+                      class="default-icon clickable mt-2"
+                      @click="onRemoveListItem(arviointi, 'arvioinnit')"/>
+                  </b-col>
+                </b-row>
+              </draggable>
+              <EpButton
+                variant="outline"
+                icon="plus"
+                @click="onAddListItem('arvioinnit')"
+                v-if="isEditing">
+                {{ $t('lisaa-arviointi') }}
+              </EpButton>
+            </template>
+            <template v-else-if="data.arvioinnit.length > 0">
+              <ul>
+                <li v-for="arviointi in data.arvioinnit" :key="arviointi.id">
+                  {{$kaanna(arviointi)}}
+                </li>
+              </ul>
+            </template>
+            <p v-else-if="!isEditing" class="font-italic">{{ $t('ei-asetettu') }}</p>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="10">
+          <b-form-group :label="$t('osaamisen-arvioinnista')">
+            <EpContent
+              v-if="isEditing || !isEditing && data.osaamisenArvioinnista"
+              v-model="data.osaamisenArvioinnista"
+              layout="normal"
+              :is-editable="isEditing"
+              :kuvaHandler="kuvaHandler"/>
+            <EpAlert v-if="!isEditing && !data.osaamisenArvioinnista" :text="$t('ei-sisaltoa')" />
+          </b-form-group>
+        </b-col>
+      </b-row>
+    </template>
   </EpEditointi>
   <EpSpinner v-else class="my-3"/>
 </template>
@@ -12,17 +226,36 @@
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 
+import * as _ from 'lodash';
+import draggable from 'vuedraggable';
+
 import { PerusteStore } from '@/stores/PerusteStore';
 import { KoulutuksenOsaStore } from '@/stores/KoulutuksenOsaStore';
+import { KuvaStore } from '@/stores/KuvaStore';
 
 import { EditointiStore } from '@shared/components/EpEditointi/EditointiStore';
 import EpSpinner from '@shared/components/EpSpinner/EpSpinner.vue';
 import EpEditointi from '@shared/components/EpEditointi/EpEditointi.vue';
+import EpKoodistoSelect from '@shared/components/EpKoodistoSelect/EpKoodistoSelect.vue'
+import { KoodistoSelectStore } from '@shared/components/EpKoodistoSelect/KoodistoSelectStore';
+import EpContent from '@shared/components/EpContent/EpContent.vue'
+import EpButton from '@shared/components/EpButton/EpButton.vue'
+import EpInput from '@shared/components/forms/EpInput.vue';
+import EpAlert from '@shared/components/EpAlert/EpAlert.vue'
+import { Koodisto, KoulutuksenOsaDtoKoulutusOsanKoulutustyyppiEnum } from '@shared/api/eperusteet'
+import { createKuvaHandler } from '@shared/components/EpContent/KuvaHandler';
+import { Kielet } from '@shared/stores/kieli';
 
 @Component({
   components: {
     EpSpinner,
     EpEditointi,
+    EpKoodistoSelect,
+    EpInput,
+    EpContent,
+    EpButton,
+    draggable,
+    EpAlert,
   }
 })
 export default class RouteKoulutuksenOsa extends Vue {
@@ -33,6 +266,17 @@ export default class RouteKoulutuksenOsa extends Vue {
   koulutuksenosaId!: number;
 
   private store: EditointiStore | null = null;
+
+  private readonly koodisto = new KoodistoSelectStore({
+    async query(query: string, sivu = 0) {
+      return (await Koodisto.kaikkiSivutettuna('koulutuksenosattuva', query, {
+        params: {
+          sivu,
+          sivukoko: 10,
+        },
+      })).data as any;
+    },
+  });
 
   @Watch('koulutuksenosaId', { immediate: true })
   async onParamChange(id: string, oldId: string) {
@@ -45,10 +289,83 @@ export default class RouteKoulutuksenOsa extends Vue {
     this.store = new EditointiStore(tkstore);
   }
 
+  isTuvaKoulutusTyyppi(type): boolean {
+    return type === this.formatKoulutusTyyppi(KoulutuksenOsaDtoKoulutusOsanKoulutustyyppiEnum.TUTKINTOKOULUTUKSEENVALMENTAVA);
+  }
+
+  onAddListItem(array): void {
+    this.store?.setData({
+      ...this.store?.data.value,
+      [array]: [
+        ..._.get(this.store?.data.value, array),
+        {},
+      ],
+    });
+  }
+
+  onRemoveListItem(rowToRemove, array) {
+    this.store?.setData({
+      ...this.store?.data.value,
+      [array]: _.filter(_.get(this.store?.data.value, array), rivi => rivi !== rowToRemove),
+    });
+  }
+
+  private formatKoulutusTyyppi(type: KoulutuksenOsaDtoKoulutusOsanKoulutustyyppiEnum): string {
+    return _.chain(type)
+      .split('_')
+      .join('')
+      .toLower()
+      .value();
+  }
+
   get perusteId() {
     return this.perusteStore.perusteId.value;
   }
 
+  get koulutusOsanKoulutustyypit(): string[] {
+    return [
+      this.formatKoulutusTyyppi(KoulutuksenOsaDtoKoulutusOsanKoulutustyyppiEnum.TUTKINTOKOULUTUKSEENVALMENTAVA),
+      this.formatKoulutusTyyppi(KoulutuksenOsaDtoKoulutusOsanKoulutustyyppiEnum.PERUSOPETUS),
+      this.formatKoulutusTyyppi(KoulutuksenOsaDtoKoulutusOsanKoulutustyyppiEnum.LUKIOKOULUTUS),
+      this.formatKoulutusTyyppi(KoulutuksenOsaDtoKoulutusOsanKoulutustyyppiEnum.AMMATILLINENKOULUTUS),
+    ]
+  }
+
+  get kuvaHandler() {
+    return createKuvaHandler(new KuvaStore(this.perusteId!));
+  }
+
+  get defaultDragOptions() {
+    return {
+      animation: 300,
+      emptyInsertThreshold: 10,
+      handle: '.order-handle',
+      disabled: !this.store!.isEditing,
+      ghostClass: 'dragged',
+    };
+  }
+
+  get tavoitteetOptions() {
+    return {
+      ...this.defaultDragOptions,
+      group: {
+        name: 'tavoitteet',
+      },
+    };
+  }
+
+  get arvioinnitOptions() {
+    return {
+      ...this.defaultDragOptions,
+      group: {
+        name: 'arvioinnit',
+      },
+    };
+  }
+
+  get sisaltokieli() {
+    return Kielet.getSisaltoKieli.value;
+  }
 }
 </script>
 
