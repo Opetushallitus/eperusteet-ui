ce36fe21bc2830cfc0edd57c3208a722bea65eed
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-02-08 14:18:16 +0200

    bugfix/EP-2630

--- src/stores/PerusteprojektiStore.ts
+++ src/stores/PerusteprojektiStore.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueCompositionApi, { reactive, computed, ref, watch } from '@vue/composition-api';
-import { getAllPerusteetInternal, PerusteHakuInternalDto, PerusteprojektiLuontiDto, Ulkopuoliset, getPerusteprojektit, PerusteprojektiKevytDto, Perusteet, Perusteprojektit, PerusteQuery, PerusteprojektiListausDto, Maintenance } from '@shared/api/eperusteet';
+import { getAllPerusteetInternal, PerusteHakuInternalDto, PerusteprojektiLuontiDto, Ulkopuoliset, getPerusteprojektit, PerusteprojektiKevytDto, Perusteet, Perusteprojektit, PerusteQuery, PerusteprojektiListausDto, Maintenance, PerusteKevytDto } from '@shared/api/eperusteet';
 import { Page } from '@shared/tyypit';
 import { IProjektiProvider } from '@/components/EpPerusteprojektiListaus/types';
 import { Debounced } from '@shared/utils/delay';
@@ -11,35 +11,19 @@ Vue.use(VueCompositionApi);
 
 export class PerusteprojektiStore {
   public state = reactive({
-    pohjat: null as PerusteHakuInternalDto[] | null,
-    perusteet: null as PerusteHakuInternalDto[] | null,
+    pohjat: null as PerusteKevytDto[] | null,
+    perusteet: null as PerusteKevytDto[] | null,
   })
 
   public readonly perusteet = computed(() => this.state.perusteet);
   public readonly pohjat = computed(() => this.state.pohjat);
 
   public async fetchPohjat() {
-    const res = (await getAllPerusteetInternal({
-      koulutusvienti: false,
-      nimi: '',
-      perusteTyyppi: 'pohja',
-      sivu: 0,
-      sivukoko: 1000,
-      tila: ['valmis'],
-    } as any)).data as Page<PerusteHakuInternalDto>;
-    this.state.pohjat = res.data;
+    this.state.pohjat = (await Perusteet.getPohjaperusteet('pohja')).data;
   }
 
   public async fetchPohjaProjektit() {
-    const res = (await getAllPerusteetInternal({
-      koulutusvienti: false,
-      nimi: '',
-      perusteTyyppi: 'normaali',
-      sivu: 0,
-      sivukoko: 1000,
-      tila: ['valmis'],
-    } as any)).data as Page<PerusteHakuInternalDto>;
-    this.state.perusteet = res.data;
+    this.state.perusteet = (await Perusteet.getPohjaperusteet('normaali')).data;
   }
 
   public async addPerusteprojekti(luontiDto: PerusteprojektiLuontiDto) {
