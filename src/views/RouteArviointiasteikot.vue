0afb40654e81098bbc808969d1e1f03ae185be30
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-03-19 14:07:02 +0200

    EP-2657-2

--- src/views/RouteArviointiasteikot.vue
+++ src/views/RouteArviointiasteikot.vue
@@ -2,38 +2,62 @@
   <div class="mt-5">
     <div class="d-flex align-items-sm-center justify-content-sm-between flex-sm-row flex-column mb-1">
       <h2 class="m-0">{{ $t('arviointiasteikot') }}</h2>
-      <ep-button
-        v-if="!isEditing"
-        variant="link"
-        icon="kyna"
-        @click="toggleEdit()"
-        class="mt-sm-0 mt-3">
-        <slot name="muokkaa">{{ $t('muokkaa') }}</slot>
-      </ep-button>
-      <div v-else class="mt-sm-0 mt-3">
-        <ep-button
-          v-if="isEditing"
-          variant="link"
-          @click="toggleEdit()">
-          <slot name="peruuta">{{ $t('peruuta') }}</slot>
-        </ep-button>
-        <ep-button
-          class="ml-2"
-          v-if="isEditing"
-          variant="primary"
-          @click="saveArviointiAsteikko()"
-          :show-spinner="isSaving">
-          <slot name="tallenna">{{ $t('tallenna') }}</slot>
-        </ep-button>
+      <div class="mt-sm-0 mt-3">
+        <div>
+          <ep-button
+            v-if="!isEditing"
+            variant="link"
+            icon="kyna"
+            @click="toggleEdit()"
+            class="mt-sm-0 mt-3"
+            v-oikeustarkastelu="{oikeus:'hallinta'}">
+            {{ $t('muokkaa') }}
+          </ep-button>
+          <ep-button
+            v-if="!isEditing"
+            variant="link"
+            icon="plus"
+            @click="lisaaArviointiasteikko()"
+            class="mt-sm-0 mt-3"
+            v-oikeustarkastelu="{oikeus:'hallinta', kohde:'pohja'}">
+          {{ $t('lisaa-uusi') }}
+          </ep-button>
+        </div>
+        <div>
+          <ep-button
+            v-if="isEditing"
+            variant="link"
+            @click="toggleEdit()">
+          {{ $t('peruuta') }}
+          </ep-button>
+          <ep-button
+            v-if="isEditing"
+            class="ml-2"
+            variant="primary"
+            @click="saveArviointiAsteikko()"
+            :show-spinner="isSaving">
+            {{ $t('tallenna') }}
+          </ep-button>
+        </div>
       </div>
     </div>
     <div class="taso-wrapper">
       <template v-if="arviointiasteikot">
         <div class="asteikko mt-4" v-for="(asteikko, idx) in arviointiasteikot" :key="asteikko.id">
           <span class="text-nowrap">
-            <h3>{{$t('arviointiasteikko') + ' ' + (idx+1)}}</h3>
-            <ep-spinner v-if="!asteikko.osaamistasot" />
-            <div v-else>
+            <div class="d-flex align-text-top justify-content-between">
+              <h3>{{$t('arviointiasteikko') + ' ' + (idx+1)}}</h3>
+              <ep-button
+                v-if="isEditing"
+                @click="poistaArviointiasteikko(asteikko)"
+                variant="link"
+                icon="roskalaatikko"
+                v-oikeustarkastelu="{oikeus:'hallinta', kohde:'pohja'}">
+                {{ $t('poista-arviointiasteikko') }}
+              </ep-button>
+            </div>
+
+            <div>
               <div
                 v-for="(taso, index) in asteikko.osaamistasot"
                 :key="taso.id"
@@ -42,15 +66,55 @@
                 <template v-if="!isEditing">
                   {{ $kaanna(taso.otsikko) }}
                 </template>
-                <b-form-group
-                  :label="$t('osaamistaso') + ' ' + (index + 1)"
-                  v-else>
-                  <ep-input
-                    :name="$t('osaamistaso')"
-                    v-model="taso.otsikko"
-                    :is-editing="true"/>
-                </b-form-group>
+                <div class="d-flex w-100 align-items-end" v-else>
+                  <b-form-group
+                    class="w-50"
+                    :label="$t('osaamistaso') + ' ' + (index + 1)">
+                    <ep-input
+                      :name="$t('osaamistaso')"
+                      v-model="taso.otsikko"
+                      :is-editing="true"/>
+                  </b-form-group>
+
+                  <b-form-group
+                    class="w-40">
+                    <ep-koodisto-select
+                      :store="koodisto"
+                      v-model="taso.koodi"
+                      :is-editing="isEditing"
+                      :naytaArvo="true">
+                    <template #default="{ open }">
+                      <b-input-group>
+                        <b-form-input
+                          :value="taso.koodi ? $kaanna(taso.koodi.nimi) : ''"
+                          disabled></b-form-input>
+                        <b-input-group-append>
+                          <b-button @click="open" icon="plus" variant="primary">
+                            {{ $t('hae-koodistosta') }}
+                          </b-button>
+                        </b-input-group-append>
+                      </b-input-group>
+                    </template>
+                  </ep-koodisto-select>
+                  </b-form-group>
+
+                  <b-form-group>
+                    <fas
+                      icon="roskalaatikko"
+                      class="default-icon clickable mb-2 ml-4"
+                      @click="poistaOsaamistaso(asteikko, taso)"
+                      v-oikeustarkastelu="{oikeus:'hallinta', kohde:'pohja'}"/>
+                  </b-form-group>
+                </div>
               </div>
+              <ep-button
+                variant="outline"
+                icon="plus"
+                @click="lisaaOsaamistaso(asteikko)"
+                v-if="isEditing"
+                v-oikeustarkastelu="{oikeus:'hallinta', kohde:'pohja'}">
+                {{ $t('lisaa-osaamistaso') }}
+              </ep-button>
             </div>
           </span>
         </div>
@@ -69,10 +133,12 @@ import EpJulkiLista from '@shared/components/EpJulkiLista/EpJulkiLista.vue';
 import EpButton from '@shared/components/EpButton/EpButton.vue';
 import EpSpinner from '@shared/components/EpSpinner/EpSpinner.vue';
 import EpInput from '@shared/components/forms/EpInput.vue';
-
+import EpKoodistoSelect from '@shared/components/EpKoodistoSelect/EpKoodistoSelect.vue';
 import { ArviointiStore } from '@/stores/ArviointiStore';
-
-import { ArviointiAsteikkoDto, OsaamistasoDto } from '@shared/api/eperusteet';
+import { ArviointiAsteikkoDto, Koodisto, OsaamistasoDto } from '@shared/api/eperusteet';
+import { KoodistoSelectStore } from '@shared/components/EpKoodistoSelect/KoodistoSelectStore';
+import * as _ from 'lodash';
+import VueScrollTo from 'vue-scrollto';
 
 @Component({
   components: {
@@ -82,13 +148,14 @@ import { ArviointiAsteikkoDto, OsaamistasoDto } from '@shared/api/eperusteet';
     EpButton,
     EpSpinner,
     EpInput,
+    EpKoodistoSelect,
   },
 })
 export default class RouteArviointiasteikot extends Vue {
   @Prop({ required: true })
   arviointiStore!: ArviointiStore;
 
-  private isEditing = false;
+  private isEditing: boolean | null = false;
   private isSaving = false;
   private arviointiasteikot: ArviointiAsteikkoDto[] | null = null;
 
@@ -103,22 +170,80 @@ export default class RouteArviointiasteikot extends Vue {
     }
   }
 
+  lisaaArviointiasteikko() {
+    this.isEditing = true;
+    this.arviointiasteikot = [
+      ...(this.arviointiasteikot || []),
+      {
+        osaamistasot: [],
+      },
+    ];
+
+    VueScrollTo.scrollTo('footer', {
+      offset: 200,
+      x: false,
+      y: true,
+    });
+  }
+
+  poistaArviointiasteikko(asteikko) {
+    this.arviointiasteikot = _.reject(this.arviointiasteikot, arviointiasteikko => arviointiasteikko === asteikko);
+  }
+
+  poistaOsaamistaso(asteikko, taso) {
+    this.arviointiasteikot = _.map(this.arviointiasteikot, arviointiasteikko => {
+      return {
+        ...arviointiasteikko,
+        osaamistasot: (asteikko === arviointiasteikko ? _.filter(asteikko.osaamistasot, osaamistaso => osaamistaso !== taso) : arviointiasteikko.osaamistasot),
+      };
+    });
+  }
+
+  lisaaOsaamistaso(asteikko) {
+    asteikko.osaamistasot = [
+      ...asteikko.osaamistasot,
+      {},
+    ];
+  }
+
   async init() {
     await this.arviointiStore.fetchArviointiasteikot();
     this.arviointiasteikot = this.arviointiStore.arviointiasteikot.value as ArviointiAsteikkoDto[];
   }
 
+  private readonly koodisto = new KoodistoSelectStore({
+    async query(query: string, sivu = 0) {
+      const { data } = await Koodisto.kaikkiSivutettuna('arviointiasteikkoammatillinen15', query, {
+        params: {
+          sivu,
+          sivukoko: 10,
+        },
+      });
+      return data as any;
+    },
+  });
+
   async saveArviointiAsteikko() {
     this.isSaving = true;
-    this.isEditing = false;
+
+    const koodittomia = _.some(this.arviointiasteikot, arviointiasteikko => {
+      return _.size(_.reject(arviointiasteikko.osaamistasot, 'koodi')) > 0;
+    });
+
+    if (koodittomia) {
+      this.$fail(this.$t('osaamistasolta-puuttuu-pakollinen-koodi') as string);
+      this.isSaving = false;
+      return;
+    }
 
     try {
       await this.arviointiStore.updateArviointiasteikot(this.arviointiasteikot as ArviointiAsteikkoDto[]);
+      this.isEditing = false;
       this.$success(this.$t('arviointiasteikko-tallennettu-onnistuneesti') as string);
+      this.init();
     }
     catch (_err) {
       this.$fail(this.$t('arviointiasteikon-tallennus-epaonnistui') as string);
-      this.init();
     }
     finally {
       this.isSaving = false;
