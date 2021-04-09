e102e695bf82021d1ba4a9f082a191683cac6d18
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-04-09 16:19:33 +0300

    EP-2737

--- src/components/EpPalautteet/EpPalautteet.vue
+++ src/components/EpPalautteet/EpPalautteet.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <template v-if="palautteet">
+      <div class="w-100 text-right">
+        <b-form-checkbox v-model="showDeleted" v-if="palautteet.length > 0" switch>
+          {{$t('nayta-arkistoidut')}}
+        </b-form-checkbox>
+      </div>
+
       <b-table
-        v-if="palautteet.length > 0"
+        v-if="palautteetFiltered.length > 0"
         striped
-        responsive
-        :items="palautteet"
+        :items="palautteetFiltered"
         :fields="fields"
         :per-page="perPage"
         :current-page="currentPage"
@@ -30,6 +35,42 @@
               :class="{ 'icon-tahti--active': rating <= item[Review.STARS] }" />
           </div>
         </template>
+        <template v-slot:cell(status)="{ item }">
+            <EpMultiSelect
+              v-if="item.status !== 'POISTETTU'"
+              :disabled="item.updating"
+              :options="tilaOptions"
+              :searchable="false"
+              :allowEmpty="false"
+              openDirection="bottom"
+              v-model="item.status"
+              @input="paivitaStatus(item, item.status)">
+              <template slot="singleLabel" slot-scope="{ option }">
+                <span class="text-nowrap">
+                  <EpColorIndicator :backgroundColor="statusColor[option]"/>
+                  <span class="ml-2">{{ $t('palaute-status-' + option.toLowerCase()) }}</span>
+                </span>
+              </template>
+              <template slot="option" slot-scope="{ option }">
+                <span class="text-nowrap">
+                  <EpColorIndicator :backgroundColor="statusColor[option]"/>
+                  <span class="ml-2">{{ $t('palaute-status-' + option.toLowerCase()) }}</span>
+                </span>
+              </template>
+
+            </EpMultiSelect>
+            <span v-else>
+              {{$t('poistettu')}}
+            </span>
+        </template>
+        <template v-slot:cell(delete)="{ item }">
+          <div v-if="item.status !== 'POISTETTU'">
+             <fas
+                icon="roskalaatikko"
+                class="default-icon clickable mt-2"
+                @click="paivitaStatus(item, 'POISTETTU')"/>
+          </div>
+        </template>
       </b-table>
       <p v-else>Ei palautteita</p>
     </template>
@@ -46,39 +87,58 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { BvTableFieldArray } from 'bootstrap-vue';
 import EpSpinner from '@shared/components/EpSpinner/EpSpinner.vue';
+import { ITPalauteProvider } from '@shared/stores/types';
+import * as _ from 'lodash';
+import { PalauteDto, PalauteDtoStatusEnum } from '@shared/api/eperusteet';
+import EpColorIndicator from '@shared/components/EpColorIndicator/EpColorIndicator.vue';
+import EpMultiSelect from '@shared/components/forms/EpMultiSelect.vue';
+import { Kielet } from '@shared/stores/kieli';
 
 enum Review {
   STARS = 'stars',
   FEEDBACK = 'feedback',
-  CREATED = 'created-at'
+  CREATED = 'created-at',
+  STATUS = 'status',
+  DELETE = 'delete',
 }
 
 @Component({
   components: {
     EpSpinner,
+    EpColorIndicator,
+    EpMultiSelect,
   },
 })
 export default class EpPalautteet extends Vue {
   @Prop({ required: true })
-  private palautteet!: any[];
+  private palautteet!: PalauteDto[];
+
+  @Prop({ required: true })
+  private palauteProvider!: ITPalauteProvider;
 
   private currentPage = 1;
   private perPage = 10;
   private sortBy = Review.CREATED;
   private sortDesc = true;
+  private showDeleted = false;
 
   ratings = Array.from({ length: 5 }, (_v, k) => k + 1);
   Review = Review;
 
   get averageReview() {
-    return (this.palautteet!.reduce((acc, curr) => acc + (curr[Review.STARS] || 0), 0) / this.palautteet!.length).toFixed(2);
+    return (this.palautteetFiltered.reduce((acc, curr) => acc + (curr[Review.STARS] || 0), 0) / this.palautteetFiltered.length).toFixed(2);
+  }
+
+  get palautteetFiltered() {
+    return _.filter(this.palautteet, palaute => this.showDeleted || palaute.status !== PalauteDtoStatusEnum.POISTETTU);
   }
 
   get fields(): BvTableFieldArray {
     return [{
       key: Review.STARS,
-      label: `${this.$t('arviointi') as string} (${this.$t('ka-keskiarvo')} ${this.averageReview})`,
+      label: `${this.$t('palaute-arviointi') as string} (${this.$t('ka-keskiarvo')} ${this.averageReview})`,
       sortable: true,
+      thStyle: { width: '200px' },
     }, {
       key: Review.FEEDBACK,
       sortable: true,
@@ -90,11 +150,72 @@ export default class EpPalautteet extends Vue {
       formatter: (_value: any, _key: string, item: any) => {
         return this.$sd(item![Review.CREATED]);
       },
+    }, {
+      key: Review.STATUS,
+      sortable: true,
+      label: this.$t('tila') as string,
+      thStyle: { width: '300px' },
+      sortByFormatted: true,
+      formatter: (_value: any, _key: string, item: any) => {
+        return this.statusSort[item.status];
+      },
+    }, {
+      key: Review.DELETE,
+      label: '',
     }];
   }
 
   get totalItems() {
-    return this.palautteet?.length || 0;
+    return this.palautteetFiltered?.length || 0;
+  }
+
+  async paivitaStatus(palaute, status) {
+    try {
+      if (status === 'POISTETTU'
+          && !(await this.$bvModal.msgBoxConfirm(
+            this.$t('vahvista-palaute-poisto') as string, {
+              title: Kielet.kaannaOlioTaiTeksti('vahvista-arkistointi'),
+              okTitle: Kielet.kaannaOlioTaiTeksti('arkistoi'),
+              cancelTitle: Kielet.kaannaOlioTaiTeksti('peruuta'),
+              cancelVariant: 'link',
+              centered: true,
+            }))) {
+        return;
+      }
+
+      await this.palauteProvider.updatePalaute?.({
+        ...palaute,
+        status,
+      });
+    }
+    catch (e) {
+      this.$fail(this.$t('virhe-palvelu-virhe') as string);
+    }
+  }
+
+  get tilaOptions() {
+    return [
+      PalauteDtoStatusEnum.SAAPUNUT,
+      PalauteDtoStatusEnum.JATKOKEHITYS,
+      PalauteDtoStatusEnum.HYLATTY,
+    ];
+  }
+
+  get statusColor() {
+    return {
+      [PalauteDtoStatusEnum.SAAPUNUT]: '#FFD900',
+      [PalauteDtoStatusEnum.JATKOKEHITYS]: '#447F13',
+      [PalauteDtoStatusEnum.HYLATTY]: '#B2B2B2',
+    };
+  }
+
+  get statusSort() {
+    return {
+      [PalauteDtoStatusEnum.SAAPUNUT]: 1,
+      [PalauteDtoStatusEnum.JATKOKEHITYS]: 2,
+      [PalauteDtoStatusEnum.HYLATTY]: 3,
+      [PalauteDtoStatusEnum.POISTETTU]: 4,
+    };
   }
 }
 </script>
--- src/stores/PalautteetStore.ts
+++ src/stores/PalautteetStore.ts
@@ -1,14 +1,18 @@
 import Vue from 'vue';
 import VueCompositionApi, { reactive, computed } from '@vue/composition-api';
 import * as _ from 'lodash';
-import { Api, PalauteDto, Palautteet } from '@shared/api/eperusteet';
+import { Api, PalauteDto, Palautteet, PalauteDtoStatusEnum } from '@shared/api/eperusteet';
 import { ITPalauteProvider, Palaute } from '@shared/stores/types';
 import { EperusteetPalautekanava } from '@shared/tyypit';
+
+interface PalauteUpdateDto extends PalauteDto {
+  updating?: boolean;
+}
 
 Vue.use(VueCompositionApi);
 export class PalautteetStore implements ITPalauteProvider {
   public state = reactive({
-    palautteet: {} as {[key: string]: PalauteDto[] | null} | null,
+    palautteet: {} as {[key: string]: PalauteUpdateDto[] | null} | null,
   })
 
   public readonly palautteet = computed(() => this.state.palautteet);
@@ -24,8 +28,10 @@ export class PalautteetStore implements ITPalauteProvider {
     });
 
     await Promise.all(_.map(Object.values(EperusteetPalautekanava), async (palautekanava) => {
+      const palauteStatukset = _.keyBy((await Palautteet.palauteStatukset(palautekanava)).data, palaute => this.palauteKey(palaute));
+
       if (_.includes(origin, 'localhost')) {
-        this.state.palautteet![palautekanava] = (((await Palautteet.palautteet(palautekanava)).data as unknown) as PalauteDto[]);
+        this.state.palautteet![palautekanava] = (((await Palautteet.palautteet(palautekanava)).data as unknown) as PalauteUpdateDto[]);
       }
       else {
         const request = Api.request({
@@ -42,14 +48,48 @@ export class PalautteetStore implements ITPalauteProvider {
             'created-at': row[0],
             stars: row[1],
             feedback: row[3],
-          } as PalauteDto;
+            key: palautekanava,
+          } as PalauteUpdateDto;
         });
       }
+
+      this.state.palautteet![palautekanava] = _.map(this.state.palautteet![palautekanava], palaute => {
+        return {
+          ...palaute,
+          status: palauteStatukset[this.palauteKey(palaute as PalauteDto)]?.status || PalauteDtoStatusEnum.SAAPUNUT,
+        };
+      });
     }));
   }
 
-  async sendPalaute(palaute: Palaute) {
+  private palauteKey(palaute: PalauteDto) {
+    return palaute.key! + (palaute['created_at'] || palaute['created-at']) + palaute.stars;
+  }
+
+  async sendPalaute(palaute: PalauteDto) {
     palaute.key = EperusteetPalautekanava.eperusteet;
     await Palautteet.sendPalaute(palaute);
+  }
+
+  async updatePalaute(updatePalaute: PalauteDto) {
+    this.setPalaute({
+      ...updatePalaute,
+      updating: true,
+    });
+    const updated = (await Palautteet.updatePalaute(updatePalaute)).data;
+    this.setPalaute({
+      ...updatePalaute,
+      updating: false,
+    });
+  }
+
+  private setPalaute(palaute: PalauteUpdateDto) {
+    this.state.palautteet![palaute.key!] = _.map(this.state.palautteet![palaute.key!], (oldPalaute: PalauteUpdateDto) => {
+      if (this.palauteKey(palaute) === this.palauteKey(oldPalaute)) {
+        return palaute;
+      }
+
+      return oldPalaute;
+    }) as PalauteUpdateDto[];
   }
 }
