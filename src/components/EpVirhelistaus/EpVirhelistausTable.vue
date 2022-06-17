6ac964833240a14521ba270591bc134417cdf16a
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2022-06-17 12:58:22 +0300

    EP-3205-4

--- src/components/EpVirhelistaus/EpVirhelistausTable.vue
+++ src/components/EpVirhelistaus/EpVirhelistausTable.vue
@@ -27,7 +27,7 @@
             <div v-if="info.validointi && info.validointi.ongelmat && info.validointi.ongelmat.length > 0" class="pt-1">
               <div v-for="ongelma in info.validointi.ongelmat" :key="ongelma.ongelma" class="ml-4 mb-2">
                 <div v-if="ongelma.ryhma">{{$kaanna(ongelma.ryhma.teksti)}}</div>
-                <div class="pl-4">{{ $t(ongelma.ongelma) }}</div>
+                <div :class="{'pl-4': ongelma.ryhma}">{{ $t(ongelma.ongelma) }}</div>
               </div>
             </div>
           </td>
