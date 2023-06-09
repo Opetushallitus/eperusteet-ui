514ddde3926956f881121c37db344ab56f810dee
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-06-09 15:21:22 +0300

    EP-3760

--- src/views/aipe/yleiset/EpAipeOppiaineenTavoite.vue
+++ src/views/aipe/yleiset/EpAipeOppiaineenTavoite.vue
@@ -62,10 +62,10 @@
 
     <b-row class="mt-4 border-bottom-2 mx-1">
       <b-col cols="4">
-        <h5>{{ $t('osaamisen-kuvaus') }}</h5>
+        <h5>{{ $t('arviointitaulukko-arvosana-otsikko') }}</h5>
       </b-col>
       <b-col cols="8">
-        <h5>{{ $t('arvion-kuvaus') }}</h5>
+        <h5>{{ $t('arviointitaulukko-osaaminen-otsikko') }}</h5>
       </b-col>
     </b-row>
 
