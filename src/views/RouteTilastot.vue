9d5fc031d47c42c11712c4cfa592744374cf2341
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-08-19 16:07:37 +0300

    EP-4312

--- src/views/RouteTilastot.vue
+++ src/views/RouteTilastot.vue
@@ -5,8 +5,15 @@
     </template>
 
     <b-tabs>
-      <b-tab :title="$t('suunnitelmien-lukumaarat')" class="mt-5">
-        <EpLukumaaraTilastot :tilastotStore="tilastotStore" />
+      <b-tab :title="$t('suunnitelmien-lukumaarat')" class="mt-3 taulukko-graafi">
+        <b-tabs pills>
+          <b-tab :title="$t('taulukko')" active>
+            <EpLukumaaraTilastot :tilastotStore="tilastotStore" class="mt-4"/>
+          </b-tab>
+          <b-tab :title="$t('graafi')">
+            <EpLukumaaraGraafit :tilastotStore="tilastotStore" class="mt-4"/>
+          </b-tab>
+        </b-tabs>
       </b-tab>
 
       <b-tab :title="$t('amosaa-tyokalu')" class="mt-5">
@@ -30,6 +37,7 @@ import { TilastotStore } from '@/stores/TilastotStore';
 import EpAmosaaTilastot from '@/components/tilastot/EpAmosaaTilastot.vue';
 import EpYlopsTilastot from '@/components/tilastot/EpYlopsTilastot.vue';
 import EpLukumaaraTilastot from '@/components/tilastot/EpLukumaaraTilastot.vue';
+import EpLukumaaraGraafit from '@/components/tilastot/EpLukumaaraGraafit.vue';
 
 @Component({
   components: {
@@ -37,6 +45,7 @@ import EpLukumaaraTilastot from '@/components/tilastot/EpLukumaaraTilastot.vue';
     EpAmosaaTilastot,
     EpYlopsTilastot,
     EpLukumaaraTilastot,
+    EpLukumaaraGraafit,
   },
 })
 export default class RouteTilastot extends Vue {
@@ -71,6 +80,24 @@ export default class RouteTilastot extends Vue {
 
 ::v-deep .container{
   max-width: 1500px;
+}
+
+::v-deep .taulukko-graafi .nav-pills .nav-item {
+  border: 2px solid #3367E3;
+
+  .nav-link {
+    border-radius: 0;
+  }
+}
+
+::v-deep .taulukko-graafi .nav-pills .nav-item:first-child {
+  border-top-left-radius: 0.5rem;
+  border-bottom-left-radius: 0.5rem;
+}
+
+::v-deep .taulukko-graafi .nav-pills .nav-item:last-child {
+  border-top-right-radius: 0.5rem;
+  border-bottom-right-radius: 0.5rem
 }
 
 </style>
