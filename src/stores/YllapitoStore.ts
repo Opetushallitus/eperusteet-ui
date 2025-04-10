2f71ccba2bb27507f3fa6e8cbe5e64a7b42c545d
Olli-Pekka Lehtokallio <53811784+oplekal@users.noreply.github.com>
2025-04-10 17:10:20 +0300

    EP-4719 (#696)

--- src/stores/YllapitoStore.ts
+++ src/stores/YllapitoStore.ts
@@ -1,11 +1,27 @@
 import Vue from 'vue';
 import VueCompositionApi, { reactive, computed } from '@vue/composition-api';
-import { Maintenance, YllapitoDto } from '@shared/api/eperusteet';
+import { Maintenance, PerusteDto, Perusteet, PerusteKevytDto, YllapitoDto } from '@shared/api/eperusteet';
 import _ from 'lodash';
 
 Vue.use(VueCompositionApi);
 
 export class YllapitoStore {
+  private state = reactive({
+    julkisivunPerusteet: null as PerusteKevytDto[] | null,
+  });
+
+  public readonly julkisivunPerusteet = computed(() => this.state.julkisivunPerusteet);
+
+  public async fetchJulkisivunPerusteet() {
+    this.state.julkisivunPerusteet = null;
+    this.state.julkisivunPerusteet = (await Perusteet.getJulkaistutKoostePerusteet()).data;
+  }
+
+  public async tallennaJulkisivunPerusteet(perusteet: PerusteKevytDto[]) {
+    await Perusteet.updateJulkaistutKoostePerusteet(perusteet);
+    this.state.julkisivunPerusteet = perusteet;
+  }
+
   public async fetch() {
     return _.sortBy((await Maintenance.getYllapidot()).data, 'key');
   }
