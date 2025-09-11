d3a9a6846596b0da90b0570b6c152834ff4151d4
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-09-11 12:10:20 +0300

    EP-4799

--- src/views/RouteMuodostuminen.vue
+++ src/views/RouteMuodostuminen.vue
@@ -508,6 +508,7 @@ export default class RouteMuodostuminen extends PerusteprojektiRoute {
       },
       clone(original) {
         return {
+          ...original,
           kuvaus: null,
           vieras: null,
           tunniste: null,
@@ -924,8 +925,9 @@ export default class RouteMuodostuminen extends PerusteprojektiRoute {
   }
 }
 
-.draggable {
+.draggable, :deep(.draggable) {
   cursor: grab;
+  user-select: none !important;
 
   &.paaryhma, &.kopioitava {
     .colorblock {
